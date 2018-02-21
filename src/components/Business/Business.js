ed498e4f1f0f0462d8b2f9c1c1f73e06842169ec
SalmonKing72 <madichjm@gmail.com>
2018-02-20 22:38:03 -0500

    adding state management of search functionality

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -15,7 +15,7 @@ class Business extends React.Component {
 			      <p>{this.props.business.city}</p>
 			      <p>{this.props.business.state} {this.props.business.zipCode}</p>
 			    </div>
-			    <div classNAme="Business-reviews">
+			    <div className="Business-reviews">
 			      <h3>{this.props.business.category.toUpperCase()}</h3>
 			      <h3 className="rating">{this.props.business.rating} stars</h3>
 			      <p>{this.props.business.reviewCount} reviews</p>
