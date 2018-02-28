7d4649b66ddf3ef383ec4f1dd34f40adb07e23d8
SalmonKing72 <madichjm@gmail.com>
2018-02-27 22:52:52 -0500

    adding in yelp api data

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,15 +4,19 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
 	render() {
-		return (
-			<div className="BusinessList">
-				{
-					this.props.businesses.map((business) => {
-						return <Business business={business}/>;
-					})
-				}
-			</div>
-		);
+		if (this.props.businesses) {
+			return (
+				<div className="BusinessList">
+					{
+						this.props.businesses.map((business) => {
+							return <Business key={business.id} business={business}/>;
+						})
+					}
+				</div>
+			);
+		} else {
+			return <div className="BusinessList"></div>
+		}
 	}
 }
 
